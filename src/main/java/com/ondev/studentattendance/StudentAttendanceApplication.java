fbb0404e0e278fe01235427711acfac03610d9b7
jean741 <iattiglah@gmail.com>
2021-04-24 23:49:52 +0200

    "new"

--- src/main/java/com/ondev/studentattendance/StudentAttendanceApplication.java
+++ src/main/java/com/ondev/studentattendance/StudentAttendanceApplication.java
@@ -1,13 +1,18 @@
 package com.ondev.studentattendance;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StudentAttendanceApplication {
-
+public class StudentAttendanceApplication extends SpringBootServletInitializer implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentAttendanceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
