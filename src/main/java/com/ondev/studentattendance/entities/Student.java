e42018796246873122986a223a60115c2628256b
jean741 <iattiglah@gmail.com>
2021-05-02 01:39:42 +0200

    "updates and build"

--- src/main/java/com/ondev/studentattendance/entities/Student.java
+++ src/main/java/com/ondev/studentattendance/entities/Student.java
@@ -3,19 +3,22 @@ package com.ondev.studentattendance.entities;
 
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
 @Entity
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
     private Long id;
+    @NonNull
     private String name;
     private String email;
 
