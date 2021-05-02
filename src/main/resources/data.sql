5634e77f550783644d7ea73a8595660bb711665b
Mohamed DIABY <mdiaby@spark.local>
2021-05-02 15:51:40 +0100

    adding data initialization scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into student(email, name) values
+("jean@gmail.com","Jean Claude"),
+("mdiaby@gmail.com","Mohamed Diaby");
+
+insert into attendance_record(student_id,reason,status,date) values
+(1,"Maladie","ABSENT",'2021-05-02'),
+(2,"Neant","PRESENT",'2021-05-02');
